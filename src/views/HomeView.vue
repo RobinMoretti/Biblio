bd3683c775c268b934102893065c6770ce9dbc60
Robin Moretti <robinhenrimoretti@gmail.com>
2024-04-09 23:23:12 +0200

    added nocodb sdk in build

---
+++ src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<script setup>
+	import { ref, onMounted,  } from 'vue'
+
+	let games = ref(process.env.data);
+	const baseUrl = ref(process.env.baseUrl)
+
+</script>
+
+<template>
+	<main>
+		<div class="game-item" v-for="game in games">
+			<img v-if="game.Image" :src="baseUrl + '/' + game.Image[0]['path']" alt="">
+			<h2>{{ game.Name }}</h2>
+			<p class="author">{{ game["Auteur - éditeur"] }}</p>
+			<p class="author">{{ game["Date de création"] }}</p>
+			<p class="author">{{ game["Spécifité"] }}</p>
+			<p class="author">{{ game["Durée"] }}</p>
+			<p class="author">{{ game["Warning"] }}</p>
+			<h3>Types: </h3>
+			<div class="types" v-for="type in game['Types'].split(',')" v-if="game['Types']">
+				<p class="type">{{ type }}</p>
+			</div>
+			<p class="author">{{ game["Difficulté"] }}</p>
+			<p class="author">{{ game["Budget"] }}</p>
+			<h3>platformes: </h3>
+			<div class="platformes" v-for="platforme in game['Platformes'].split(',')" v-if="game['Platformes']">
+				<p class="Platforme">{{ platforme }}</p>
+			</div>
+			<h3>styles: </h3>
+			<div class="styles" v-for="style in game['Styles'].split(',')" v-if="game['Styles']">
+				<p class="Style">{{ style }}</p>
+			</div>
+		</div>
+	</main>
+</template>
+
+<style scoped>
+	main{
+		display: grid;
+		grid-template-columns: 33.3% 33.3% 33.3%;
+		padding: 0 2rem;
+		border: solid 1px black;
+		width: 100%;
+	}
+
+	.game-item{
+		border: solid 1px black;
+		padding: 1rem;
+		margin: 1rem;
+	}
+	/* .game-item > *{
+		max-width: 100%;
+	} */
+	
+	.game-item img{
+		width: 100%;
+	}
+
+	.types{
+		margin-left: 10px;
+	}
+	
+</style>
---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+import { Api } from 'nocodb-sdk'
+
+const api = new Api({
+  baseURL: 'https://databases.robinmoretti.eu',
+  headers: {
+	'xc-token': 'qUcPHD5V41Oo-HzTHEa6UTmBlY88BgdrAB63pjw3'
+  }
+})
+
+// console.log("weeessshhhh")
+
+// https://vitejs.dev/config/
+export default defineConfig(async ({ command, mode}) => {
+//   const data = await asyncFunction()
+  const data = await getData();
+  
+  return {
+		plugins: [
+			vue(),
+			VueDevTools(),
+		],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url))
+			}
+		},
+		define: { 'process.env': { data: data, baseUrl: "https://databases.robinmoretti.eu"} },
+	}
+})
+
+
+async function getData() {
+	const response = await api.dbViewRow.list(
+		"noco",
+		"pr14d1a80kphz3g",
+		"mygz3a7xx4omeos",
+		"vwtie9l5kkrboyat", {
+			"offset": 0,
+			"limit": 25,
+			"where": ""
+	})
+	return response.list;
+}
