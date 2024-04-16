8ca683a9a5d9cfa9ef72bec7e2d5fc54553d0ab9
Robin Moretti <robinmoretti@MacBook-Pro-de-Robin-2.local>
2024-04-16 21:43:07 +0200

    update home page

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,22 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <!-- <RouterLink to="/about">About</RouterLink> -->
-      </nav>
-    </div>
+	<!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+	<div class="wrapper">
+	  <nav>
+		<RouterLink to="/">Home</RouterLink>
+	  </nav>
+	</div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.wrapper{
+	background-color: white;
+	display: flex;
+	padding: 2rem 0;
+	font-size: 1.5rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,23 @@
+<!-- eslint-disable no-undef -->
 <script setup>
-	import { ref, onMounted,  } from 'vue'
+	import { ref } from 'vue'
 
 	let games = ref(process.env.data);
 	const baseUrl = ref(process.env.baseUrl)
+	
+	console.log(games.value[0]);
+
+	games.value.forEach(game => {
+		game.types = game['Types'].split(',');
+		game.platformes = game['Platformes'].split(',');
+		game.styles = game['Styles'].split(',');
+	});
 
 </script>
 
 <template>
 	<main>
-		<div class="game-item" v-for="game in games">
+		<div class="game-item" v-for="(game, key) in games" v-bind:key="'game-' + key">
 			<img v-if="game.Image" :src="baseUrl + '/' + game.Image[0]['path']" alt="">
 			<h2>{{ game.Name }}</h2>
 			<p class="author">{{ game["Auteur - éditeur"] }}</p>
@@ -16,25 +25,35 @@
 			<p class="details">{{ game["Spécifité"] }}</p>
 			<p class="time">{{ game["Durée"] }}</p>
 			<p class="warning">{{ game["Warning"] }}</p>
-			<h3>Types: </h3>
-			<div class="types" v-for="type in game['Types'].split(',')" v-if="game['Types']">
-				<p class="type">{{ type }}</p>
+
+			<div class="types-container" v-if="game.types.length">
+				<h3>Types: </h3>
+				<div class="types" v-for="(type, key) in game.types" v-bind:key="'type-' + key">
+					<p class="type">{{ type }}</p>
+				</div>
 			</div>
-			<p class="author">{{ game["Difficulté"] }}</p>
-			<p class="author">{{ game["Budget"] }}</p>
-			<h3>platformes: </h3>
-			<div class="platformes" v-for="platforme in game['Platformes'].split(',')" v-if="game['Platformes']">
-				<p class="Platforme">{{ platforme }}</p>
+
+			<p class="difficulty">{{ game["Difficulté"] }}</p>
+			<p class="budget">{{ game["Budget"] }}</p>
+
+			<div class="platformes-container" v-if="game.platformes">
+				<h3>platformes: </h3>
+				<div class="platformes" v-for="(platforme, key) in game.platforms" v-bind:key="'platform-' + key">
+					<p class="Platforme">{{ platforme }}</p>
+				</div>
 			</div>
-			<h3>styles: </h3>
-			<div class="styles" v-for="style in game['Styles'].split(',')" v-if="game['Styles']">
-				<p class="Style">{{ style }}</p>
-			</div>
+
+			<div class="styles-container" v-if="game.styles">
+				<h3>styles: </h3>
+				<div class="styles" v-for="(style, key) in game.styles" v-bind:key="'style-' + key">
+					<p class="Style">{{ style }}</p>
+				</div>
+			</div>	
 		</div>
 	</main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 	main{
 		display: grid;
 		grid-template-columns: 33.3% 33.3% 33.3%;
