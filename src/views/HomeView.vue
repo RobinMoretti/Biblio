080705b2d5c82aeadaba38a6425f44f39005a6a9
Robin Moretti <robinhenrimoretti@gmail.com>
2024-04-18 20:37:52 +0200

    almost finished basic data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ const baseUrl = ref(process.env.baseUrl);
 
 games.value.forEach((game) => {
     if (game["Date de création"]) {
+        console.log(game["Date de création"]);
         const dateOptions = {
             year: "numeric",
             month: "2-digit",
@@ -19,9 +20,17 @@ games.value.forEach((game) => {
         );
     }
 
-    game["Types"] ? (game.types = game["Types"].split(",")) : null;
-    game["Platformes"] ? (game.platformes = game["Platformes"].split(",")) : null;
-    game["Styles"] ? (game.styles = game["Styles"].split(",")) : null;
+    if (game["Types"]) {
+        game["Types"] = game["Types"].replaceAll(",", ", ");
+        // game["Types"] ? (game.types = game["Types"].split(",")) : null;
+        //game["Types"] ? (game.types = game["Types"].replace(",", " ,")) : null;
+    }
+
+    game["Difficulté"] ? (game.difficulty = game["Difficulté"].split(",")) : null;
+    game["Platformes"]
+        ? (game["Platformes"] = game["Platformes"].replaceAll(",", ", "))
+        : null;
+    game["Styles"] ? (game["Styles"] = game["Styles"].replaceAll(",", ", ")) : null;
 });
 </script>
 
@@ -29,54 +38,64 @@ games.value.forEach((game) => {
     <main>
         <div class="game-item" v-for="(game, key) in games" v-bind:key="'game-' + key">
             <img v-if="game.Image" :src="baseUrl + '/' + game.Image[0]['path']" alt="" />
-            <div class="content">
+            <header>
                 <h2>{{ game.Name }}</h2>
                 <p class="created-date">{{ game["Date de création"] }}</p>
                 <p class="author">{{ game["Auteur - éditeur"] }}</p>
-                <p class="details">{{ game["Spécifité"] }}</p>
-                <p class="time">{{ game["Durée"] }}</p>
-                <p class="warning">{{ game["Warning"] }}</p>
+            </header>
+            <div class="content">
+                <p class="details row">{{ game["Spécifité"] }}</p>
+                <p class="time row">
+                    <span class="left-column title">Durée :</span>
+                    <span class="right-column">{{ game["Durée"] }}</span>
+                </p>
+
+                <p class="warning row" v-if="game.Warning">
+                    <span class="left-column title">⚠️</span>
+                    <span class="right-column">{{ game.Warning }}</span>
+                </p>
 
                 <div
-                    class="types-container tags-container"
-                    v-if="game.types && game.types.length"
+                    class="types-container tags-container row"
+                    v-if="game.Types && game.Types.length"
                 >
-                    <h3 class="title">Types :</h3>
-                    <span
+                    <h3 class="left-column title">Types :</h3>
+
+                    <span class="right-column">{{ game["Types"] }}</span>
+                    <!-- <span
                         class="type tag"
                         v-for="(type, key) in game.types"
                         v-bind:key="'type-' + key"
                     >
                         {{ type }}
-                    </span>
+                    </span> -->
                 </div>
 
-                <p class="difficulty">{{ game["Difficulté"] }}</p>
-                <p class="budget">{{ game["Budget"] }}</p>
+                <p class="difficulty row" v-if="game.difficulty">
+                    <span class="left-column title">Difficulté :</span>
+                    <span class="right-column">{{ game["Difficulté"] }}</span>
+                </p>
+                <p class="budget row" v-if="game.Budget">
+                    <span class="left-column title">Budget :</span>
+                    <span class="right-column">{{ game.Budget }}€</span>
+                </p>
 
                 <div
-                    class="platformes-container"
-                    v-if="game.platformes && game.platformes"
+                    class="platformes-container row"
+                    v-if="game.Platformes && game.Platformes.length"
                 >
-                    <h3>platformes:</h3>
-                    <div
-                        class="platformes"
-                        v-for="(platforme, key) in game.platforms"
-                        v-bind:key="'platform-' + key"
-                    >
-                        <p class="Platforme">{{ platforme }}</p>
-                    </div>
+                    <h3 class="left-column title">Platformes :</h3>
+
+                    <span class="right-column">{{ game.Platformes }}</span>
                 </div>
 
-                <div class="styles-container" v-if="game.styles && game.styles">
-                    <h3>styles:</h3>
-                    <div
-                        class="styles"
-                        v-for="(style, key) in game.styles"
-                        v-bind:key="'style-' + key"
-                    >
-                        <p class="Style">{{ style }}</p>
-                    </div>
+                <div
+                    class="styles-container row last"
+                    v-if="game.Styles && game.Styles.length"
+                >
+                    <h3 class="left-column title">Styles :</h3>
+
+                    <span class="right-column">{{ game.Styles }}</span>
                 </div>
             </div>
         </div>
